a52593f5e580fc736a95af3dd1ddb46ef7e73268
Beataklys <beata.klyss@gmail.com>
2016-10-11 23:27:52 +0200

    Customising association

--- app/controllers/actors/comments_controller.rb
+++ app/controllers/actors/comments_controller.rb
@@ -2,6 +2,6 @@ class Actors::CommentsController< CommentsController
   before_action :set_commentable
   private
   def set_commentable
-    @commentable= Actor.find{params[:actor_id]}
+    @commentable= Actor.find(params[:actor_id])
   end
 end
--- app/controllers/films/comments_controller.rb
+++ app/controllers/films/comments_controller.rb
@@ -2,6 +2,6 @@ class Films::CommentsController< CommentsController
   before_action :set_commentable
   private
   def set_commentable
-    @commentable= Film.find{params[:film_id]}
+    @commentable= Film.find(params[:film_id])
   end
 end
